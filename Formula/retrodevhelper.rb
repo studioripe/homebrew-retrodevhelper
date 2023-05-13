4a754717f9ffe577bde2537426a784769aca88a9
Joshua Mangiola <joshm982@gmail.com>
2023-05-13 21:27:48 +0930

    Update retrodevhelper.rb

--- Formula/retrodevhelper.rb
+++ Formula/retrodevhelper.rb
@@ -1,12 +1,9 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Retrodevhelper < Formula
   desc "Helper tool for Retro Game Development Written in Rust"
   homepage "https://github.com/studioripe/retrodevhelper/"
-  url "https://github.com/studioripe/retrodevhelper/releases/download/0.0.1/retrodevhelper-mac.tar.gz"
-  sha256 "7d5ecba435dc764bb8f2b157ff1c0b27f7a050d18690f36c9a8de62de9bd70ad"
-  version "0.0.1"
+  url "https://github.com/studioripe/retrodevhelper/releases/download/v0.0.2/retrodevhelper-0.0.2-macos-universal.tar.gz"
+  sha256 "9d9cf8b5faa67b3cc5af1b8b03a9f72eeed7905e0d2acfc47aaf8fd2d18bf0cf"
+  version "0.0.2"
 
   def install
     bin.install "retrodevhelper"
