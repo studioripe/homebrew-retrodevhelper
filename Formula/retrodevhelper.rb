52f24af07d5d99e68f2094699649f2c63ee4666e
Joshua Mangiola <joshm982@gmail.com>
2023-05-13 19:07:52 +0930

    Update retrodevhelper.rb

--- Formula/retrodevhelper.rb
+++ Formula/retrodevhelper.rb
@@ -4,7 +4,7 @@
 class Retrodevhelper < Formula
   desc "Helper tool for Retro Game Development Written in Rust"
   homepage "https://github.com/studioripe/retrodevhelper/"
-  url "https://github.com/studioripe/retrodevhelper/releases/latest/download/retrodevhelper-mac.tar.gz"
+  url "https://github.com/studioripe/retrodevhelper/releases/download/0.0.1/retrodevhelper-mac.tar.gz"
   sha256 "7d5ecba435dc764bb8f2b157ff1c0b27f7a050d18690f36c9a8de62de9bd70ad"
   version "0.0.1"
 
